ac3995040bd4c332cb6fa6bb77e49711a332669d
xezrunner <8061077+xezrunner@users.noreply.github.com>
2025-07-10 19:42:21 +0200

    Stopwatch: WIP: initial window border

--- StopwatchOnMac/StopwatchOnMac.swift
+++ StopwatchOnMac/StopwatchOnMac.swift
@@ -3,9 +3,36 @@
 import Foundation
 import SwiftUI
 
+// MARK: - Miscellaneous
+
 // Logs as "functionName(): message"
 internal func Log(_ format: String, _ functionName: String = #function, _ args: any CVarArg...) {
     Foundation.NSLog("\(functionName)(): \(format)", args)
+}
+
+internal enum ButtonInteractionState { case idle, hovering, pressed }
+
+// Styling:
+
+internal struct StopwatchWindowBorder: View {
+    var body: some View {
+        let endRadius = 700.0
+        
+        let randomX = CGFloat.random(in: -endRadius...endRadius) / endRadius
+        let randomY = CGFloat.random(in: -endRadius...endRadius) / endRadius
+        let gradient = RadialGradient(stops: [
+            Gradient.Stop(color: .white, location: 1),
+            Gradient.Stop(color: .clear, location: 0),
+        ], center: UnitPoint(x: randomX, y: randomY), startRadius: 0, endRadius: endRadius)
+        
+        RoundedRectangle(cornerRadius: 16)
+            .strokeBorder(gradient, lineWidth: 0.8)
+//            .strokeBorder(gradient, lineWidth: 240)
+            .ignoresSafeArea()
+            .compositingGroup()
+            .blendMode(.plusLighter)
+            .blur(radius: 1.3)
+    }
 }
 
 extension View {
@@ -14,12 +41,9 @@ extension View {
             .preferredColorScheme(.dark) // TODO: we don't necessarily want to force dark mode, but visionOS does seem more like dark mode
         
             .containerBackground(.thinMaterial, for: .window)
+            .overlay { StopwatchWindowBorder() }
         
             .buttonStyle(StopwatchButtonStyle())
             .environment(\.font, Font.system(size: 15))
     }
 }
-
-// MARK: - Miscellaneous
-
-internal enum ButtonInteractionState { case idle, hovering, pressed }
