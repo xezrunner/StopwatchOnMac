ec46b61684de7e34b672ec770b88762501e8a1ca
xezrunner <8061077+xezrunner@users.noreply.github.com>
2025-07-14 16:10:37 +0200

    Project: refactor files

--- StopwatchOnMac/Controls/StopwatchNavigation/StopwatchNavigationSplitView.swift
+++ StopwatchOnMac/Controls/StopwatchNavigation/StopwatchNavigationSplitView.swift
@@ -40,7 +40,7 @@ public struct StopwatchNavigationSplitView<Selection: Hashable, Sidebar: View, C
                     .environmentObject(selectionStore)
             }
             .padding(12)
-            .frame(maxWidth: 300, maxHeight: .infinity, alignment: .top)
+            .frame(maxWidth: 340, maxHeight: .infinity, alignment: .top)
             
             // TODO: Content!
             
@@ -118,7 +118,7 @@ extension View {
     }
 }
 
-// MARK: - Navigation links:
+// MARK: - Miscellaneous:
 
 internal class StopwatchNavigationSelectionStore<Selection: Hashable>: ObservableObject {
     @Published var selection: Selection? {
@@ -142,125 +142,3 @@ internal class StopwatchNavigationSelectionStore<Selection: Hashable>: Observabl
     var view: (() -> AnyView)?
 }
 
-public struct StopwatchNavigationLink<Destination: View, Label: View, Value: Hashable>: View {
-    @Environment(\.stopwatchButtonStyleConfiguration) private var buttonStyleConfigurationEnvironment
-
-    @EnvironmentObject var selectionStore: StopwatchNavigationSelectionStore<Value>
-    
-    @Environment(SWNavigationViewLinkage.self) var viewLinkage
-    var destination: (() -> Destination)?
-    
-    var label: () -> Label
-    
-    var value: Value?
-    // If no value is given, generate a unique ID for each link and use it as selection:
-    @State var _destinationImplicitID: Value?
-    
-    private func navigate() {
-        if let destination = destination {
-            viewLinkage.view = { AnyView(destination()) }
-        } else { print("StopwatchNavigationLink: no destination, skipping navigation") }
-        
-        if let value = value ?? _destinationImplicitID { selectionStore.selection = value }
-    }
-    
-    var isSelected: Bool {
-        if let selection = selectionStore.selection, let value = value ?? _destinationImplicitID {
-            return selection == value
-        }
-        return false
-    }
-    
-    private var buttonStyleConfiguration: StopwatchButtonStyleConfiguration {
-        buttonStyleConfigurationEnvironment ?? .navigationLinkInList
-    }
-    
-    private var selectedStyleConfiguration: StopwatchButtonStyleConfiguration {
-        var configuration: StopwatchButtonStyleConfiguration = buttonStyleConfiguration
-        
-        configuration.shapeIdleOpacity = 0.25
-        
-        return configuration
-    }
-    
-    public var body: some View {
-        Button(action: navigate, label: label)
-            .stopwatchButtonStyleConfiguration(!isSelected ? buttonStyleConfiguration : selectedStyleConfiguration)
-    }
-}
-
-extension StopwatchNavigationLink {
-    // FIXME: these crash due to not having selectionStore<Never>!
-    public init(_ titleKey: LocalizedStringKey) where Label == Text, Value == Never, Destination == Never {
-        self.label = { Text(titleKey) }
-    }
-    
-    public init(title: LocalizedStringKey, iconSystemName: String) where Label == SwiftUI.Label<Text, Image>, Value == Never, Destination == Never {
-        self.label = { SwiftUI.Label(title, systemImage: iconSystemName) }
-    }
-    
-    @_disfavoredOverload
-    public init<S: StringProtocol>(_ titleKey: S) where Label == Text, Value == Never, Destination == Never {
-        self.label = { Text(titleKey) }
-    }
-    
-    // MARK: - Destination-based:
-    public init(@ViewBuilder destination: @escaping () -> Destination, @ViewBuilder label: @escaping () -> Label) where Value == UUID {
-        self.destination = destination
-        self._destinationImplicitID = UUID()
-        self.label = label
-    }
-    
-    @_disfavoredOverload // https://forums.swift.org/t/how-to-determine-if-a-passed-argument-is-a-string-literal/41651/6
-    public init<S: StringProtocol>(_ titleKey: S, @ViewBuilder destination: @escaping () -> Destination) where Label == Text, Value == UUID {
-        self.init(destination: destination, label: { Text(titleKey) })
-    }
-    
-    public init(_ titleKey: LocalizedStringKey, @ViewBuilder destination: @escaping () -> Destination) where Label == Text, Value == UUID {
-        self.init(destination: destination, label: { Text(titleKey) })
-    }
-    
-    // MARK: - Value-based:
-    public init(value: Value, @ViewBuilder label: @escaping () -> Label) where Destination == Never {
-        self.value = value
-        self.label = label
-    }
-    
-    @_disfavoredOverload
-    public init<S: StringProtocol>(_ titleKey: S, value: Value) where Label == Text, Destination == Never {
-        self.init(value: value, label: { Text(titleKey) })
-    }
-    
-    public init(_ titleKey: LocalizedStringKey, value: Value) where Label == Text, Destination == Never {
-        self.init(value: value, label: { Text(titleKey) })
-    }
-}
-
-extension StopwatchButtonStyleConfiguration {
-    public static var sidebar: StopwatchButtonStyleConfiguration {
-        var styleConfig = transparent
-        
-        // TODO: unify roundness!
-        styleConfig.shape = RoundedRectangle(cornerRadius: 12.0, style: .circular)
-        styleConfig.shapeHoverOpacity = 0.1
-        styleConfig.shapePressedOpacity = 0.25
-        
-        styleConfig.alignment = .leading
-        styleConfig.maxWidth = .infinity
-        styleConfig.padding = (20.0, 14.0)
-        styleConfig.pressedScale = 1.0
-        
-        styleConfig.labelIconSize = 26.0
-        
-        return styleConfig
-    }
-    
-    internal static var navigationLinkInList: StopwatchButtonStyleConfiguration {
-        var styleConfig = sidebar
-        
-        styleConfig.shape = Rectangle()
-        styleConfig.maxHeight = .infinity
-        
-        return styleConfig
-    }
-}
